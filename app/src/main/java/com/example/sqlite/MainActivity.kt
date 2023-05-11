e05a058b0ff9d4603acb3b9389c146d5da4d8303
prince <22013993@student.ciu.edu.tr>
2023-05-11 16:12:41 +0300

    new functions added

--- app/src/main/java/com/example/sqlite/MainActivity.kt
+++ app/src/main/java/com/example/sqlite/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     private lateinit var edWord:EditText
@@ -19,6 +20,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         init();
+        additem();
     }
     fun init(){
         edWord = findViewById(R.id.edWord)
@@ -26,5 +28,19 @@ class MainActivity : AppCompatActivity() {
         btnAdd = findViewById(R.id.btnAdd)
         btnView = findViewById(R.id.btnView)
         sqliteHelper = SqliteHelper(this)
+    }
+    fun additem(){
+        val word = edWord.text.toString()
+        val mean = edMean.text.toString()
+        if (word.isNullOrEmpty() || mean.isNullOrEmpty())
+            Toast.makeText(this, "Please enter the fields", Toast.LENGTH_LONG).show()
+        else{
+            var itm = ItemModel(word = word, mean = mean)
+           val status = sqliteHelper.insertItem(itm)
+            if (status < 0)
+                Toast.makeText(this, "Record is not saved", Toast.LENGTH_LONG).show()
+            else
+                Toast.makeText(this, "Record saved", Toast.LENGTH_LONG).show()
+        }
     }
 }
